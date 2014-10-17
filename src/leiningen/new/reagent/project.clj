1befb2a7c572befd1720551fdc5144e577922714
Yogthos <dmitri.sotnikov@gmail.com>
2014-10-17 08:23:56 -0400

    added cljs-ajax

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -8,6 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [cljs-ajax "0.3.3"]
                  [ring "1.3.1"]
                  [compojure "1.2.0"]
                  [lib-noir "0.9.3"]
