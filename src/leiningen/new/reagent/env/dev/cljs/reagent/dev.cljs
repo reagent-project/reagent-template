4f0df9c23fe803ab623848d480ca884890cfeed9
Yogthos <dmitri.sotnikov@gmail.com>
2015-02-17 09:14:00 -0500

    updated dev.cljs to not reload with Figwheel

--- src/leiningen/new/reagent/env/dev/cljs/reagent/dev.cljs
+++ src/leiningen/new/reagent/env/dev/cljs/reagent/dev.cljs
@@ -1,4 +1,4 @@
-(ns {{project-ns}}.dev
+(ns ^:figwheel-no-load {{project-ns}}.dev
   (:require [{{project-ns}}.core :as core]
             [figwheel.client :as figwheel :include-macros true]
             [weasel.repl :as weasel]
