6d290c3e40a9bb9f7ec546d17d49ad7f988d19e2
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-01 08:38:15 -0400

    bumped up reagent-forms

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -10,7 +10,7 @@
                  [ring-server "0.4.0"]
                  [cljsjs/react "0.13.1-0"]
                  [reagent "0.5.0"]
-                 [reagent-forms "0.4.8"]
+                 [reagent-forms "0.4.9"]
                  [reagent-utils "0.1.4"]
                  [org.clojure/clojurescript "0.0-3169" :scope "provided"]
                  [ring "1.3.2"]
