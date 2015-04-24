41096616ff4774554753304b2475ce57fafbe970
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-24 18:31:15 -0400

    updated piggieback version

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -56,7 +56,7 @@
                                   [leiningen "2.5.1"]
                                   [figwheel "0.2.6"]
                                   [weasel "0.6.0"]
-                                  [com.cemerick/piggieback "0.2.0"]
+                                  [com.cemerick/piggieback "0.2.1"]
                                   [org.clojure/tools.nrepl "0.2.10"]
                                   [pjstadig/humane-test-output "0.7.0"]{{{dev-dependencies}}}]
 
