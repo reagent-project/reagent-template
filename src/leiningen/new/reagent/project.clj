499e6fcc9fd5cef1673f1e6619f84d52301a60c8
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-05-13 14:03:46 -0400

    Update project.clj

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -10,7 +10,7 @@
                  [ring-server "0.4.0"]
                  [cljsjs/react "0.13.1-0"]
                  [reagent "0.5.0"]
-                 [reagent-forms "0.5.0"]
+                 [reagent-forms "0.5.1"]
                  [reagent-utils "0.1.4"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
