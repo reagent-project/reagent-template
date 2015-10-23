c489b7f845cbe0554f9b83d26316d2e2af94915c
Yogthos <dmitri.sotnikov@gmail.com>
2015-10-23 09:13:40 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent/lein-template "0.9.22"
+(defproject reagent/lein-template "0.9.23"
   :description "A Leiningen template for a ClojureScript setup with Figwheel, Austin, and Reagent."
   :url "https://github.com/reagent-project/reagent-template"
   :license {:name "MIT License"
