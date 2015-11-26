67c257705a34ccf610d8e62fce0b9506e64d965c
Yogthos <dmitri.sotnikov@gmail.com>
2015-11-26 09:43:33 -0500

    bumped up lein-asset-minifier

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -25,7 +25,7 @@
 
   :plugins [[lein-environ "1.0.1"]
             [lein-cljsbuild "1.1.1"]
-            [lein-asset-minifier "0.2.2"
+            [lein-asset-minifier "0.2.4"
              :exclusions [org.clojure/clojure]]]
 
   :ring {:handler {{project-ns}}.handler/app
@@ -75,9 +75,9 @@
                                    :exclusions [org.clojure/clojure org.clojure/tools.reader]]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                  {{#devcards-hook?}} 
+                                  {{#devcards-hook?}}
                                   [devcards "0.2.0-8"
-                                   :exclusions [org.clojure/tools.reader]] 
+                                   :exclusions [org.clojure/tools.reader]]
                                   {{/devcards-hook?}}
                                   [pjstadig/humane-test-output "0.7.0"]
                                   {{dev-dependencies}}]
