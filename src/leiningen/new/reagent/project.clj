b4a19bad2506ecedeabdc2231a5118077a1b3c25
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-11 19:45:55 -0500

    formatting

--- src/leiningen/new/reagent/project.clj
+++ src/leiningen/new/reagent/project.clj
@@ -15,7 +15,8 @@
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
                  [environ "1.0.2"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.228"
+                  :scope "provided"]
                  [secretary "1.2.3"]
                  [venantius/accountant "0.1.6"
                   :exclusions [org.clojure/tools.reader]]]
