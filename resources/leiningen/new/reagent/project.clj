0f8f050c643a0b56223727593d8efc4a371aa42e
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2017-02-10 16:40:11 -0500

    bumped up clojurescript

--- resources/leiningen/new/reagent/project.clj
+++ resources/leiningen/new/reagent/project.clj
@@ -13,7 +13,7 @@
                  [compojure "1.5.1"]
                  [hiccup "1.0.5"]
                  [yogthos/config "0.8"]
-                 [org.clojure/clojurescript "1.9.293"
+                 [org.clojure/clojurescript "1.9.473"
                   :scope "provided"]
                  [secretary "1.2.3"]
                  [venantius/accountant "0.1.7"
