e5897b3e543d116b503c0d41982d1fba52bbb3de
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-02-20 20:23:03 -0500

    added js symbol printing for dev

--- resources/leiningen/new/reagent/env/dev/cljs/reagent/dev.cljs
+++ resources/leiningen/new/reagent/env/dev/cljs/reagent/dev.cljs
@@ -3,6 +3,11 @@
     [{{project-ns}}.core :as core]
     [devtools.core :as devtools]))
 
+(extend-protocol IPrintWithWriter
+  js/Symbol
+  (-pr-writer [sym writer _]
+    (-write writer (str "\"" (.toString sym) "\""))))
+
 (devtools/install!)
 
 (enable-console-print!)
