ee1d814e846cbcfa41fca1c1f67df01eb8eebfdc
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-05-18 09:44:25 -0400

    bumped up dependencies

--- resources/leiningen/new/reagent/project.clj
+++ resources/leiningen/new/reagent/project.clj
@@ -8,15 +8,16 @@
                  [ring-server "0.5.0"]
                  [reagent "0.10.0"]
                  [reagent-utils "0.3.3"]
-                 [ring "1.8.0"]
+                 [ring "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
                  [hiccup "1.0.5"]
                  [yogthos/config "1.1.7"]
-                 [org.clojure/clojurescript "1.10.741"
+                 [org.clojure/clojurescript "1.10.764"
                   :scope "provided"]{{#shadow-cljs-hook?}}
                  [com.google.javascript/closure-compiler-unshaded "v20190325"]
                  [org.clojure/google-closure-library "0.0-20190213-2033d5d9"]{{/shadow-cljs-hook?}}
-                 [metosin/reitit "0.4.2"]
+                 [metosin/reitit "0.5.1"]
+                 [metosin/jsonista "0.2.6"]
                  {{#clerk-hook?}}
                  [pez/clerk "1.0.0"]
                  {{/clerk-hook?}}
@@ -133,12 +134,12 @@
   {{/sass-hook?}}
 
   :profiles {:dev {:repl-options {:init-ns {{project-ns}}.repl}
-                   :dependencies [[cider/piggieback "0.4.2"]
+                   :dependencies [[cider/piggieback "0.5.0"]
                                   [binaryage/devtools "1.0.0"]
                                   [ring/ring-mock "0.4.0"]
-                                  [ring/ring-devel "1.8.0"]
+                                  [ring/ring-devel "1.8.1"]
                                   [prone "2020-01-17"]
-                                  [figwheel-sidecar "0.5.19"]
+                                  [figwheel-sidecar "0.5.20"]
                                   [nrepl "0.7.0"]
                                   {{#shadow-cljs-hook?}}
                                   [thheller/shadow-cljs "2.8.37"]
@@ -161,7 +162,7 @@
                                   {{/sass-hook?}} ]
 
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.19"]
+                   :plugins [[lein-figwheel "0.5.20"]
                              {{#test-hook?}}
                              [lein-doo "0.1.10"]
                              {{/test-hook?}}
